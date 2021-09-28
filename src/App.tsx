2b5f136ac4db21198fce78f368af4c8010d03811
dila93 <sebas.dila@gmail.com>
2021-09-28 10:06:55 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import './App.css'
+import Circle from './components/Circle';
+
+export default function App() {
+  return (
+    <main>
+      <Circle/>
+    </main>
+  )
+}
---
+++ src/components/Circle.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './circle.css'
+
+export default function Circle() {
+  return (
+    <>
+		<div className="circle">hola</div>
+    </>
+  )
+}
